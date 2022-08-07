7618f52b2345c88ae7b3d72661f644bb034405ec
losie2 <dlsgh1347@naver.com>
2022-08-08 00:11:33 +0900

    Code update

--- OpenCV/OpenCV.cpp
+++ OpenCV/OpenCV.cpp
@@ -24,6 +24,284 @@ const float GOOD_PORTION = 0.15f;
 int64 work_begin = 0;
 int64 work_end = 0;
 
+/*
+    뷰 영상 출력을 위한 구조체
+*/
+struct Axis
+{
+    int x;
+    int y;
+    int z;
+};
+struct AxisDouble
+{
+    double x;
+    double y;
+    double z;
+};
+
+Mat imgView;
+Mat imgPanorama;
+double HorizontalViewAngle = 1.5f;
+double VerticalViewAngle = 1.2f;
+int ViewSize = 500;
+
+double yAxisRotate = 0;
+double zAxisRotate = 0;
+
+Axis getNearPixel(Mat Img, int x, int y);
+Mat remapImage(Mat imgIn);
+AxisDouble CalcThetaPhiToXYZ(double theta, double phi);
+AxisDouble CalcCubicXYZ(double x, double y, double z);
+void GenView(Mat SourceImg);
+void KeyDownEvent(int ch, Mat SourceImg);
+void InsertClip(Mat SourceImg);
+
+//#######
+//Image Processing
+//#######
+Axis getNearPixel(Mat Img, int x, int y)
+{
+	int col = 1;
+	int row = 1;
+	bool done = false;
+	Axis xy;
+
+	while (!done)
+	{
+		for (int i = -col; i <= col; i++)
+		{
+			for (int j = -row; j <= row; j++)
+			{
+				if (x + i <= 0 || y + j <= 0)
+				{
+					continue;
+				}
+				if (x + i >= Img.cols || y + j >= Img.rows)
+				{
+					continue;
+				}
+				// (3, 3) 범위 내에서 3채널에 모두 보간.
+				if (!(Img.at<Vec3b>(Point(x + i, y + j))[0] == 0 && Img.at<Vec3b>(Point(x + i, y + j))[1] == 0 && Img.at<Vec3b>(Point(x + i, y + j))[2] == 0))
+				{
+					xy.x = x + i;
+					xy.y = y + j;
+					return xy;
+				}
+			}
+		}
+		col++;
+		row++;
+
+		if (col > 3 || row > 3)
+		{
+			xy.x = x;
+			xy.y = y;
+			return xy;
+		}
+	}
+}
+Mat remapImage(Mat imgIn)
+{
+	int width = imgIn.cols;
+	int height = imgIn.rows;
+	Mat RemapImg = Mat(height, width, CV_8UC3, Scalar(0, 0, 0));
+
+	for (int i = 0; i < width; i++)
+	{
+		for (int j = 0; j < height; j++)
+		{
+			Axis xy = getNearPixel(imgIn, i, j);
+			RemapImg.at<Vec3b>(Point(i, j)) = imgIn.at<Vec3b>(Point(xy.x, xy.y));
+		}
+	}
+
+	return RemapImg;
+
+}
+
+
+//#######
+//Generate View
+//#######
+void GenView(Mat SourceImg)
+{
+	int width = SourceImg.cols;
+	int height = SourceImg.rows;
+
+	int Multiple = 2; // 연산의 속도를 위함
+	double radius = height / 2 / Multiple; // CubemapEdgeLength / 2, Normalization => -1 ~ 0, 0 ~ 1
+
+	int viewWidth = (int)((double)height * HorizontalViewAngle / Multiple);
+	int viewHeight = (int)((double)height * VerticalViewAngle / Multiple);
+
+	imgView = Mat(viewHeight, viewWidth, CV_8UC3, Scalar(0, 0, 0));
+	cv::resize(SourceImg, imgPanorama, Size(width, height), 0, 1);
+
+
+	for (int i = 0; i < width; i++)
+	{
+		for (int j = 0; j < height; j++)
+		{
+			Vec3b pixel = SourceImg.at<Vec3b>(Point(i, j));
+
+			double theta = (((double)j / height) * PI) - (PI * 0.5f);	//-pi/2 ~ pi/2
+			double phi = (((double)i / width) * 2 * PI) - PI;			//-pi ~ pi
+
+			AxisDouble axis = CalcThetaPhiToXYZ(theta, phi);
+
+			if (axis.x > 0)
+			{
+				/*
+					전체 구면 좌표계 내에서 x > 0 인 평면만 고려,
+					회전 변환을 거친 후 좌표계에는 변동이 없다는 점을 이용.
+				*/
+				AxisDouble axis2;
+				axis2 = CalcCubicXYZ(axis.x, axis.y, axis.z);
+
+				/*
+					Transformation normalization Cubemap Coordinate to Image Viewer Pixel Coordinate
+					Length of edge on YZ-Face
+
+				*/
+				int y = (int)(axis2.y * radius) + viewWidth / 2;
+				int z = (int)(axis2.z * radius) + viewHeight / 2;
+
+				if (y >= 0 && y < viewWidth && z >= 0 && z < viewHeight)
+				{
+
+					int PointX = y;
+					int PointY = z;
+
+					if (PointX < 5 || PointX > viewWidth - 6 || PointY < 5 || PointY > viewHeight - 6)
+					{
+						Vec3b C = { 255, 0, 0 }; // BGR
+						imgPanorama.at<Vec3b>(Point(i, j)) = C;
+					}
+
+					imgView.at<Vec3b>(Point(PointX, PointY)) = pixel;
+
+				}
+			}
+		}
+	}
+	imgView = remapImage(imgView);
+	cv::resize(imgView, imgView, Size(ViewSize, (int)((double)ViewSize / (HorizontalViewAngle / VerticalViewAngle))), 0, 1);
+	cv::imshow("Panorama", imgPanorama);
+	cv::imshow("View", imgView);
+
+	int ch = waitKey();
+	KeyDownEvent(ch, SourceImg);
+}
+AxisDouble CalcThetaPhiToXYZ(double theta, double phi)
+{
+	AxisDouble axis;
+
+	//theta, phi -> x,y,z 변환
+	double x1 = cos(theta) * cos(phi);
+	double y1 = cos(theta) * sin(phi);
+	double z1 = sin(theta);
+
+	//z축 회전 변환
+	double x2 = x1 * cos(zAxisRotate) - y1 * sin(zAxisRotate);
+	double y2 = x1 * sin(zAxisRotate) + y1 * cos(zAxisRotate);
+	double z2 = z1;
+
+	//y축 회전 변환
+	double x3 = x2 * cos(yAxisRotate) + z2 * sin(yAxisRotate);
+	double y3 = y2;
+	double z3 = -x2 * sin(yAxisRotate) + z2 * cos(yAxisRotate);
+
+	axis.x = x3;
+	axis.y = y3;
+	axis.z = z3;
+
+	return axis;
+}
+void KeyDownEvent(int ch, Mat SourceImg)
+{
+	switch (ch)
+	{
+	case 's'://Theta Up
+		//if (thetaPlus <= PI / 2)
+	{
+		yAxisRotate += 5 * (PI / 365);
+		//if (thetaPlus > PI / 2)
+		{
+			//	thetaPlus = PI / 2;
+		}
+		cout << "Lat : " << -yAxisRotate << ", Long : " << -zAxisRotate << endl;
+	}
+
+	GenView(SourceImg);
+	break;
+
+	case 'w'://Theta Down
+		//if (thetaPlus >= -PI / 2)
+	{
+		yAxisRotate -= 5 * (PI / 365);
+		//if (thetaPlus < -PI / 2)
+		{
+			//	thetaPlus = -PI / 2;
+		}
+		cout << "Lat : " << -yAxisRotate << ", Long : " << -zAxisRotate << endl;
+	}
+
+	GenView(SourceImg);
+	break;
+
+	case 'd'://Phi Down
+		zAxisRotate -= 5 * (2 * PI / 365);
+
+		if (zAxisRotate <= 0)
+		{
+			zAxisRotate = 2 * PI;
+		}
+		cout << "Lat : " << -yAxisRotate << ", Long : " << -zAxisRotate << endl;
+
+		GenView(SourceImg);
+		break;
+
+	case 'a'://Phi Up
+		zAxisRotate += 5 * (2 * PI / 365);
+		if (zAxisRotate >= 2 * PI)
+		{
+			zAxisRotate = 0;
+		}
+		cout << "Lat : " << -yAxisRotate << ", Long : " << -zAxisRotate << endl;
+
+		GenView(SourceImg);
+		break;
+
+	default:
+		GenView(SourceImg);
+		break;
+	}
+}
+AxisDouble CalcCubicXYZ(double x, double y, double z)
+{
+	AxisDouble axis;
+
+	/*
+	큐브맵화, 항상 Front Face에서 렌더링 하기 때문에 단위벡터는 x이다. .
+	*/
+	double a = x;
+	double x1 = x / a;
+	double y1 = y / a;
+	double z1 = z / a;
+
+	axis.x = x1;
+	axis.y = y1;
+	axis.z = z1;
+
+	return axis;
+}
+
+
+/*
+	1. Spherical -> Cubemap
+	2. Cubemap -> Spherical
+*/
 float* GetCubemapCoordinate(int x, int y, int face, int edge, float* point)
 {
 	/*
@@ -285,6 +563,10 @@ Mat CvtSph2Cub(Mat* pano) {
     }
     return cubemap;
 }
+
+/*
+	SURF
+*/
 static void workBegin()
 {
 	work_begin = getTickCount();
@@ -395,7 +677,6 @@ static Mat drawGoodMatches(
 
 	return img_matches;
 }
-
 ////////////////////////////////////////////////////
 // This program demonstrates the usage of SURF_OCL.
 // use cpu findHomography interface to calculate the transformation matrix
@@ -406,20 +687,19 @@ int main(int argc, char* argv[])
 
     /* 파노라마 이미지 불러오기 */
     Mat img = imread("Panorama.jpg"); //자신이 저장시킨 이미지 이름이 입력되어야 함, 확장자까지
-    Mat srcImage;
-    Mat grayMat;
+    Mat srcImage; 
     cv::resize(img, srcImage, Size(img.cols*1.5, img.rows*1.5), 0, 0);
     
 
      /* Mat 선언 */
     Mat SphericalToCubemap;
-    //Mat CubemapToSpherical;
-   // CubemapToSpherical = Mat::zeros(img.size().height, img.size().width, img.type());
 
     /* 변환함수 */
     SphericalToCubemap = CvtSph2Cub(&srcImage);
 
 	std::string outpath = "output.jpg";
+    std::string result = "result.jpg";
+
 	std::string leftName = "object.jpg";
 
 	imread(leftName, IMREAD_COLOR).copyTo(img1);
@@ -477,8 +757,8 @@ int main(int argc, char* argv[])
 	Mat img_matches = drawGoodMatches(img1.getMat(ACCESS_READ), img2.getMat(ACCESS_READ), keypoints1, keypoints2, matches, corner);
 
 	//-- Show detected matches
-	namedWindow("surf matches", WINDOW_AUTOSIZE);
-	imshow("surf matches", img_matches);
+	//namedWindow("surf matches", WINDOW_AUTOSIZE);
+	//imshow("surf matches", img_matches);
 	imwrite(outpath, img_matches);
 
 
@@ -487,8 +767,15 @@ int main(int argc, char* argv[])
 	line(img2, corner[1], corner[2], Scalar(0, 255, 0), 2, LINE_AA);
 	line(img2, corner[2], corner[3], Scalar(0, 255, 0), 2, LINE_AA);
 	line(img2, corner[3], corner[0], Scalar(0, 255, 0), 2, LINE_AA);
-	imshow("draw square", img2);
+	//imshow("draw square", img2);
+    imwrite(result, img2);
 
+    Mat matched_Cubemap = imread("result.jpg");
+    Mat resultSph = CvtCub2Sph(&matched_Cubemap, &srcImage);
+
+    //imshow("spherical Image", resultSph);
+
+	GenView(resultSph);
 
 	waitKey(0);
 	return EXIT_SUCCESS;
